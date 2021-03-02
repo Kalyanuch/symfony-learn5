a87b789d0d60743b56bac86fba9008ca2c3ab3f1
Skalyan <itlutsk@gmail.com>
2021-03-02 09:32:09 +0200

    Transfer gallery data from conteroller into template 2021-03-02

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,9 +10,37 @@ class IndexController extends AbstractController
     public function home()
     {
         //return new Response('<h1>Hello world!!</h1>');
+        $images = [
+            [
+                'href' => 'images/hotel/intro_room.jpg',
+                'class' => 'hidesm',
+                'alt' => 'Intro Gallery Room Sample Pictures'
+            ],
+            [
+                'href' => 'images/hotel/intro_pool.jpg',
+                'class' => '',
+                'alt' => 'Intro Gallery Pool Sample Pictures'
+            ],
+            [
+                'href' => 'images/hotel/intro_dining.jpg',
+                'class' => '',
+                'alt' => 'Intro Gallery Dining Sample Pictures'
+            ],
+            [
+                'href' => 'images/hotel/intro_attractions.jpg',
+                'class' => '',
+                'alt' => 'Intro Gallery Attractions Sample Pictures'
+            ],
+            [
+                'href' => 'images/hotel/intro_wedding.jpg',
+                'class' => 'hidesm',
+                'alt' => 'Intro Gallery Dining Sample Pictures'
+            ],
+        ];
 
         return $this->render('index.html.twig', [
-            'year' => random_int(0, 100)
+            'year' => random_int(0, 100),
+            'images' => $images
         ]);
     }
 
